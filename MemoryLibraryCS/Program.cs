546c93eb7bfad9d7fb44d77c71e33611f35e4ded
Table <table@DESKTOP-QN5FKUK>
2022-01-31 15:47:41 -0700

    Project Changes

--- MemoryLibraryCS/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using MemoryLibraryCS.Library;
-
-MemoryManager manager = new("CPPTesting");
-
-Console.WriteLine(manager.Read<int>(new(0x040A1B6F8A4)));
-manager.Write(new(0x040A1B6F8A4), 0x69);
-Console.WriteLine(manager.Read<int>(new(0x040A1B6F8A4)));
